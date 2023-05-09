c16b76e5eb4cbd86714c57067ff974466612c8d6
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-05-09 17:18:05 +0530

    impliment update and delete function

--- src/components/UpdateAlbum.js
+++ src/components/UpdateAlbum.js
@@ -1,3 +1,5 @@
+/////////
+
 import React from 'react'
 import { Link } from "react-router-dom";
 import Navbar from './Navbar';
@@ -43,3 +45,5 @@ const UpdateAlbum = (props) => {
 }
 
 export default UpdateAlbum;
+
+/////////
